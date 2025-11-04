98c4fa82137d7c5ffbec0e06032334a4c111a18b
Dipanshu Sharma <dipanshusharma7015@gmail.com>
2025-11-04 13:11:41 +0530

    update

--- Assignment-JAVA-first-bit-solution/Using set and this keyword example in Java/admin.java
+++ Assignment-JAVA-first-bit-solution/Using set and this keyword example in Java/admin.java
@@ -1,43 +1,36 @@
-class SalesManager
-{
-	int id;
-	String name;
-	double salary;
-	double incentive;
-	double target;
+class Admin {
+   	   int id;
+ 	   String name;
+   	   double salary;
+  	   double allowance;
 
-	void setID(int i){
-		this.id = i;
-	}
-	void setName(String n){
-		this.name = n;
-	}
-	void setSalary(double s){
-		this.salary = s;	
-	}
-	void setIncentive(double i){
-		this.incentive = i;
-	}
-	void setTarget(double t){
-		this.target = t;
-	}
-}//sales manager info store here 
-class MangerInf0
-{
-	public static void main(String[] args){
-		SalesManager s1;
-		s1 = new SalesManager();
+    void setId(int i) {
+        this.id = i;
+    }
+    void setName(String n) {
+        this.name = n;
+    }
+    void setSalary(double s) {
+        this.salary = s;
+    }
+    void setAllowance(double a) {
+        this.allowance = a;
+    }
+}//admin info store here 
 
-		s1.setID(102);
-		s1.setName("Rahul");
-		s1.setSalary(2000.0);
-		s1.setIncentive(300.50);
-		s1.setTarget(100);
+class AdminInfo {
+    public static void main(String[] args) {
+        Admin a1;
+        a1 = new Admin();
 
-	System.out.println("ID is : " + s1.id);
-	System.out.println("Name is : " + s1.name);
-	System.out.println("Salary is : " + s1.salary);
-	System.out.println("Incentive is : " + s1.incentive);
-	System.out.println("Target is : " + s1.target);
-	}//main ends here
-}//info store here 
+        a1.setId(101);
+        a1.setName("Rohit");
+        a1.setSalary(35000.0);
+        a1.setAllowance(5000.0);
+
+        System.out.println("Admin ID: " + a1.id);
+        System.out.println("Admin Name: " + a1.name);
+        System.out.println("Admin Salary: " + a1.salary);
+        System.out.println("Admin Allowance: " + a1.allowance);
+    }//main ends here 
+}//end here of this code 
